84ffbc677fc921ab9fe70bdcfa3618f18528d951
rmbits <rmwebstack@gmail.com>
2020-07-14 07:16:24 -0400

    update

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -75,170 +75,169 @@ export default [{
     id: 1,
     email: 'acaine0@loc.gov'
   }]
+},
+{
+  id: 4,
+  firstName: 'Herbert',
+  lastName: 'Melanaphy',
+  phones: [{
+    id: 1,
+    phone: '692-372-2589'
+  }, {
+    id: 2,
+    phone: '173-686-9269'
+  }, {
+    id: 3,
+    phone: '659-131-6466'
+  }],
+  addresses: [{
+    id: 1,
+    address: '6 Bashford Junction'
+  }],
+  emails: [{
+    id: 1,
+    email: 'cspitaro0@jigsy.com'
+  }, {
+    id: 2,
+    email: 'aechallier1@fema.gov'
+  }]
+},
+{
+  id: 5,
+  firstName: 'Dotti',
+  lastName: 'Joriot',
+  phones: [{
+    id: 1,
+    phone: '678-275-1158'
+  }, {
+    id: 2,
+    phone: '494-604-7315'
+  }],
+  addresses: [{
+    id: 1,
+    address: '56 Clove Center'
+  }, {
+    id: 2,
+    address: '49 Northview Crossing'
+  }],
+  emails: [{
+    id: 1,
+    email: 'dbenard0@wikipedia.org'
+  }]
+},
+{
+  id: 6,
+  firstName: 'Gavrielle',
+  lastName: 'Burge',
+  phones: [{
+    id: 1,
+    phone: '734-343-0394'
+  }],
+  addresses: [{
+    id: 1,
+    address: '05070 Dovetail Avenue'
+  }, {
+    id: 2,
+    address: '5483 Division Trail'
+  }],
+  emails: [{
+    id: 1,
+    email: 'krothery0@cisco.com'
+  }, {
+    id: 2,
+    email: 'rdugdale1@biblegateway.com'
+  }]
+},
+{
+  id: 7,
+  firstName: 'Angelo',
+  lastName: 'Streetley',
+  phones: [{
+    id: 1,
+    phone: '231-480-8019'
+  }],
+  addresses: [{
+    id: 1,
+    address: '61139 Holmberg Avenue'
+  }],
+  emails: [{
+    id: 1,
+    email: 'ftwinterman0@goo.ne.jp'
+  }, {
+    id: 2,
+    email: 'roshea1@telegraph.co.uk'
+  }]
+},
+{
+  id: 8,
+  firstName: 'Leonid',
+  lastName: 'Oldham',
+  phones: [{
+    id: 1,
+    phone: '209-987-5630'
+  }],
+  addresses: [{
+    id: 1,
+    address: '64589 Northport Way'
+  }, {
+    id: 2,
+    address: '9239 Melvin Junction'
+  }],
+  emails: [{
+    id: 1,
+    email: 'awaterson0@samsung.com'
+  }, {
+    id: 2,
+    email: 'oblackborne1@barnesandnoble.com'
+  }, {
+    id: 3,
+    email: 'rranahan2@mail.ru'
+  }]
+},
+{
+  id: 9,
+  firstName: 'Karrah',
+  lastName: 'Greneham',
+  phones: [{
+    id: 1,
+    phone: '783-813-8692'
+  }],
+  addresses: [{
+    id: 1,
+    address: '2171 Corben Place'
+  }, {
+    id: 2,
+    address: '7428 Spohn Parkway'
+  }],
+  emails: [{
+    id: 1,
+    email: 'etowe0@sourceforge.net'
+  }]
+},
+{
+  id: 10,
+  firstName: 'Maddalena',
+  lastName: 'Dulwitch',
+  phones: [{
+    id: 1,
+    phone: '152-689-8606'
+  }, {
+    id: 2,
+    phone: '842-496-3589'
+  }],
+  addresses: [{
+    id: 1,
+    address: '67782 Buell Junction'
+  }, {
+    id: 2,
+    address: '5377 Lighthouse Bay Way'
+  }],
+  emails: [{
+    id: 1,
+    email: 'lruppeli0@i2i.jp'
+  }, {
+    id: 2,
+    email: 'wfishbie1@e-recht24.de'
+  }]
 }
-// ,
-// {
-//   id: 4,
-//   firstName: 'Herbert',
-//   lastName: 'Melanaphy',
-//   phones: [{
-//     id: 1,
-//     phone: '692-372-2589'
-//   }, {
-//     id: 2,
-//     phone: '173-686-9269'
-//   }, {
-//     id: 3,
-//     phone: '659-131-6466'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '6 Bashford Junction'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'cspitaro0@jigsy.com'
-//   }, {
-//     id: 2,
-//     email: 'aechallier1@fema.gov'
-//   }]
-// },
-// {
-//   id: 5,
-//   firstName: 'Dotti',
-//   lastName: 'Joriot',
-//   phones: [{
-//     id: 1,
-//     phone: '678-275-1158'
-//   }, {
-//     id: 2,
-//     phone: '494-604-7315'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '56 Clove Center'
-//   }, {
-//     id: 2,
-//     address: '49 Northview Crossing'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'dbenard0@wikipedia.org'
-//   }]
-// },
-// {
-//   id: 6,
-//   firstName: 'Gavrielle',
-//   lastName: 'Burge',
-//   phones: [{
-//     id: 1,
-//     phone: '734-343-0394'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '05070 Dovetail Avenue'
-//   }, {
-//     id: 2,
-//     address: '5483 Division Trail'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'krothery0@cisco.com'
-//   }, {
-//     id: 2,
-//     email: 'rdugdale1@biblegateway.com'
-//   }]
-// },
-// {
-//   id: 7,
-//   firstName: 'Angelo',
-//   lastName: 'Streetley',
-//   phones: [{
-//     id: 1,
-//     phone: '231-480-8019'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '61139 Holmberg Avenue'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'ftwinterman0@goo.ne.jp'
-//   }, {
-//     id: 2,
-//     email: 'roshea1@telegraph.co.uk'
-//   }]
-// },
-// {
-//   id: 8,
-//   firstName: 'Leonid',
-//   lastName: 'Oldham',
-//   phones: [{
-//     id: 1,
-//     phone: '209-987-5630'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '64589 Northport Way'
-//   }, {
-//     id: 2,
-//     address: '9239 Melvin Junction'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'awaterson0@samsung.com'
-//   }, {
-//     id: 2,
-//     email: 'oblackborne1@barnesandnoble.com'
-//   }, {
-//     id: 3,
-//     email: 'rranahan2@mail.ru'
-//   }]
-// },
-// {
-//   id: 9,
-//   firstName: 'Karrah',
-//   lastName: 'Greneham',
-//   phones: [{
-//     id: 1,
-//     phone: '783-813-8692'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '2171 Corben Place'
-//   }, {
-//     id: 2,
-//     address: '7428 Spohn Parkway'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'etowe0@sourceforge.net'
-//   }]
-// },
-// {
-//   id: 10,
-//   firstName: 'Maddalena',
-//   lastName: 'Dulwitch',
-//   phones: [{
-//     id: 1,
-//     phone: '152-689-8606'
-//   }, {
-//     id: 2,
-//     phone: '842-496-3589'
-//   }],
-//   addresses: [{
-//     id: 1,
-//     address: '67782 Buell Junction'
-//   }, {
-//     id: 2,
-//     address: '5377 Lighthouse Bay Way'
-//   }],
-//   emails: [{
-//     id: 1,
-//     email: 'lruppeli0@i2i.jp'
-//   }, {
-//     id: 2,
-//     email: 'wfishbie1@e-recht24.de'
-//   }]
-// }
 ]
